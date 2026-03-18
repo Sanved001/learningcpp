8a69005df6a1e944c4970731e2d8aae26165f13b
sanved <166846665+Sanved001@users.noreply.github.com>
2026-03-18 18:16:33 +0530

    First Commit YAY!

---
+++ TicTacToe.cpp
@@ -0,0 +1,19 @@
+//
+// Created by sanved on 3/11/26.
+//
+
+/*
+ * # Illustration:
+ * - - - || {p1_name} | {p2_name}
+ * - - - || {p1_Score}| {p2_score}
+ * - - - || T: {rounds_left}
+ *
+ * # Planning:
+ * Hmm......Y E A H
+ * I have no idea how to plan alright
+ */
+
+
+
+
+
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <list>
+
+
+class Player {
+    public:
+        int* player_position_x;
+        int* player_position_y;
+        list* player_position_list(int player_position_x,int player_position_y);
+
+
+};
+
+
+
+int main() {
+
+}
+
+
+
